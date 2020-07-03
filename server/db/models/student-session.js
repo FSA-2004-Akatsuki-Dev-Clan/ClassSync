d3dd149fff2819e31efa85d2d4122c2e9482af6b
bpoole1989 <55624907+bpoole1989@users.noreply.github.com>
2020-07-03 10:55:58 -0400

    Update student-session.js

--- server/db/models/student-session.js
+++ server/db/models/student-session.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const StudentSession = db.define('studentSession', {
-
+  data: {
+    type: Sequelize.INTEGER
+  }
 })
 
 module.exports = StudentSession
