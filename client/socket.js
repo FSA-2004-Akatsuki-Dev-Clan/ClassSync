52e7a3acc00a06b5101d3f36765a05e2862b4c68
Charlies Pan <Cp81089@gmail.com>
2020-07-03 23:03:09 -0400

    feat: getUserMedia through sockets

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,28 @@
 import io from 'socket.io-client'
 
-const socket = io(window.location.origin)
+// const socket = io(window.location.origin)
+const socket = io('http://localhost:8080')
+
+//for ngrok
+// const socket = io('https://ded537cde722.ngrok.io')
 
 socket.on('connect', () => {
   console.log('Connected!')
+})
+
+socket.on('user-devices-client', function() {
+  let mediaConfigObj = {
+    audio: true,
+    video: true
+  }
+  navigator.mediaDevices
+    .getUserMedia(mediaConfigObj)
+    .then(function(mediaStreamObj) {
+      console.log('connected!', mediaStreamObj)
+    })
+    .catch(function(err) {
+      console.log('ohh noooo!!', err)
+    })
 })
 
 export default socket
--- server/socket/index.js
+++ server/socket/index.js
@@ -5,5 +5,9 @@ module.exports = io => {
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
+
+    socket.on('user-devices-client', function() {
+      socket.broadcast.emit('user-devices-client')
+    })
   })
 }
