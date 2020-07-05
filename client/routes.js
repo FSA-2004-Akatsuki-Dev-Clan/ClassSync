64091e619cbe7d0ab4164912294aa69a90466e87
Barish <barpoole@gmail.com>
2020-07-05 11:43:16 -0400

    feat: usermedia page can detect faces on webcam and count the words spoken

--- client/routes.js
+++ client/routes.js
@@ -22,6 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/usermedia" component={UserMedia} />
+        {/* <Route path="/session" component={Session} /> */}
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
