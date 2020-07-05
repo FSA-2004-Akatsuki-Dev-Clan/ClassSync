fea5e045c98c95063288b0c09d278bf62a4a8b00
bpoole1989 <55624907+bpoole1989@users.noreply.github.com>
2020-07-05 17:46:38 -0400

    Update sessions.js

--- server/db/models/sessions.js
+++ server/db/models/sessions.js
@@ -6,7 +6,7 @@ const Session = db.define('sessions', {
     type: Sequelize.STRING
   },
   activityType: {
-    type: Sequelize.ENUM('pair-work', 'group-work', 'reading', 'writing', 'test', 'quiz', 'discussion')
+    type: Sequelize.ENUM('lecture', 'pair-work', 'group-work', 'reading', 'writing', 'test', 'quiz', 'discussion')
   },
   details: {
     type: Sequelize.TEXT
