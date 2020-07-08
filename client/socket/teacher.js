1f3cb34adf4c4d24f30e2aca7cc7c586ccebcca6
Barish <barpoole@gmail.com>
2020-07-08 09:08:05 -0400

    feat: server socket listeners added, sessionData storage implemented

--- client/socket/teacher.js
+++ client/socket/teacher.js
@@ -2,21 +2,39 @@ import socket from '.'
 
 const teacherSocket = socket
 
-teacherSocket.on('cancel', (id, first, last) => {
+export const startSession = (teacherId, sessionDetails) => {
+  if (window.confirm('Are you ready to start the session?')) {
+    teacherSocket.emit('start-session', teacherId, sessionDetails)
+
+    document.getElementById('start').hidden = true
+    document.getElementById('end').hidden = false
+  }
+}
+
+export const endSession = () => {
+  if (window.confirm('Are you sure you want to end the session?')) {
+    teacherSocket.emit('end-session')
+
+    document.getElementById('start').hidden = false
+    document.getElementById('end').hidden = true
+  }
+}
+
+teacherSocket.on('cancel', (socketId, studentId, first, last) => {
   if (
     window.confirm(
-      `Student ${first} ${last}, ID ${id} did not accept session start. Send another invitation?`
+      `Student ${first} ${last}, ID ${studentId} did not accept session start. Send another invitation?`
     )
   )
-    socket.emit('re-invite', id)
+    socket.emit('re-invite', socketId)
   else {
     const reInvite = document.createElement('button')
-    reInvite.innerHTML = `Re-Invite Student: ${first} ${last}, ID ${id}`
+    reInvite.innerHTML = `Re-Invite Student: ${first} ${last}, ID ${studentId}`
 
     document.getElementById('teacher-session').appendChild(reInvite)
 
     reInvite.onclick = () => {
-      socket.emit('re-invite', id)
+      socket.emit('re-invite', socketId)
       document.getElementById('teacher-session').removeChild(reInvite)
     }
   }
@@ -26,22 +44,12 @@ teacherSocket.on('data', (id, data) => {
   console.log(`current data for student '${id}': `, data)
 })
 
-export const startSession = id => {
-  if (window.confirm('Are you ready to start the session?')) {
-    teacherSocket.emit('start-session', id)
+teacherSocket.on('student-disconnect', id => {
+  window.alert(`Student disconnected from server, ID: ${id}`)
+})
 
-    document.getElementById('start').hidden = true
-    document.getElementById('end').hidden = false
-  }
-}
-
-export const endSession = id => {
-  if (window.confirm('Are you sure you want to end the session?')) {
-    teacherSocket.emit('end-session', id)
-
-    document.getElementById('start').hidden = false
-    document.getElementById('end').hidden = true
-  }
-}
+teacherSocket.on('reconnected', () => {
+  console.log('Reconnected to the server!')
+})
 
 export default teacherSocket
