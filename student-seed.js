a25a25e5ba14725bce4d16ef23c81301d5e14a22
Charlies Pan <Cp81089@gmail.com>
2020-07-13 21:50:12 -0400

    feat: seeded instead of making comp

--- student-seed.js
+++ student-seed.js
@@ -4,183 +4,420 @@ const studentSeed = [
     lastName: 'Parkes',
     gradeLevel: 5,
     email: 'pierreparkes1@gmail.com',
-    password: '123'
+    password: '123',
+    faceCountAvg: 88,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 70,
+    wordsSpokenAvg: 380,
+    mouseClickAvg: 44,
+    keyStrokeAvg: 342,
+    numberOfSessions: 10
   },
   {
     firstName: 'Omar',
     lastName: 'Martin',
     gradeLevel: 5,
     email: 'omarmartin1@gmail.com',
-    password: '123'
+    password: '123',
+    faceCountAvg: 90,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 85,
+    wordsSpokenAvg: 230,
+    mouseClickAvg: 78,
+    keyStrokeAvg: 210,
+    numberOfSessions: 10
   },
   {
     firstName: 'Milly',
     lastName: 'Cain',
     gradeLevel: 5,
     email: 'millycain1@gmail.com',
-    password: '123'
+    password: '123',
+    faceCountAvg: 85,
+    faceAttemptAvg: 122,
+    faceScoreAvg: 70,
+    wordsSpokenAvg: 102,
+    mouseClickAvg: 50,
+    keyStrokeAvg: 142,
+    numberOfSessions: 10
   },
   {
     firstName: 'Lauryn',
     lastName: 'Ridley',
     gradeLevel: 5,
-    email: 'laurynridley1@gmail.com'
+    email: 'laurynridley1@gmail.com',
+    password: '123',
+    faceCountAvg: 79,
+    faceAttemptAvg: 120,
+    faceScoreAvg: 66,
+    wordsSpokenAvg: 160,
+    mouseClickAvg: 23,
+    keyStrokeAvg: 321,
+    numberOfSessions: 10
   },
   {
     firstName: 'Kuba',
     lastName: 'Davey',
     gradeLevel: 5,
-    email: 'kubadavey1@gmail.com'
+    email: 'kubadavey1@gmail.com',
+    password: '123',
+    faceCountAvg: 75,
+    faceAttemptAvg: 120,
+    faceScoreAvg: 63,
+    wordsSpokenAvg: 170,
+    mouseClickAvg: 54,
+    keyStrokeAvg: 222,
+    numberOfSessions: 10
   },
   {
     firstName: 'Holli',
     lastName: 'Snow',
     gradeLevel: 5,
-    email: 'hollisnow1@gmail.com'
+    email: 'hollisnow1@gmail.com',
+    password: '123',
+    faceCountAvg: 70,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 65,
+    wordsSpokenAvg: 130,
+    mouseClickAvg: 65,
+    keyStrokeAvg: 134,
+    numberOfSessions: 10
   },
   {
     firstName: 'Kaidan',
     lastName: 'Patrick',
     gradeLevel: 5,
-    email: 'kaidanpatrick1@gmail.com'
+    email: 'kaidanpatrick1@gmail.com',
+    password: '123',
+    faceCountAvg: 85,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 90,
+    wordsSpokenAvg: 302,
+    mouseClickAvg: 89,
+    keyStrokeAvg: 240,
+    numberOfSessions: 10
   },
   {
     firstName: 'Amelia-Lily',
     lastName: 'Bean',
     gradeLevel: 5,
-    email: 'amelia-lilybean1@gmail.com'
+    email: 'amelia-lilybean1@gmail.com',
+    password: '123',
+    faceCountAvg: 73,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 92,
+    wordsSpokenAvg: 197,
+    mouseClickAvg: 67,
+    keyStrokeAvg: 340,
+    numberOfSessions: 10
   },
   {
     firstName: 'Pearce',
     lastName: 'Fitzgerald',
     gradeLevel: 5,
-    email: 'pearcefitzgerald1@gmail.com'
+    email: 'pearcefitzgerald1@gmail.com',
+    password: '123',
+    faceCountAvg: 85,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 72,
+    wordsSpokenAvg: 176,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 189,
+    numberOfSessions: 10
   },
   {
     firstName: 'Kye',
     lastName: 'Brook',
     gradeLevel: 5,
-    email: 'kyebrook1@gmail.com'
+    email: 'kyebrook1@gmail.com',
+    password: '123',
+    faceCountAvg: 84,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 92,
+    wordsSpokenAvg: 324,
+    mouseClickAvg: 91,
+    keyStrokeAvg: 280,
+    numberOfSessions: 10
   },
   {
     firstName: 'Israr',
     lastName: 'Field',
     gradeLevel: 5,
-    email: 'israrfield1@gmail.com'
+    email: 'israrfield1@gmail.com',
+    password: '123',
+    faceCountAvg: 83,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 91,
+    wordsSpokenAvg: 289,
+    mouseClickAvg: 93,
+    keyStrokeAvg: 264,
+    numberOfSessions: 10
   },
   {
     firstName: 'Safiya',
     lastName: 'Cresswell',
     gradeLevel: 5,
-    email: 'safiyacresswell1@gmail.com'
+    email: 'safiyacresswell1@gmail.com',
+    password: '123',
+    faceCountAvg: 74,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 81,
+    wordsSpokenAvg: 267,
+    mouseClickAvg: 91,
+    keyStrokeAvg: 259,
+    numberOfSessions: 10
   },
   {
     firstName: 'Roland',
     lastName: 'Casey',
     gradeLevel: 5,
-    email: 'rolandcasey1@gmail.com'
+    email: 'rolandcasey1@gmail.com',
+    password: '123',
+    faceCountAvg: 93,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 91,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 85,
+    keyStrokeAvg: 307,
+    numberOfSessions: 10
   },
   {
     firstName: 'Claude',
     lastName: 'Campos',
     gradeLevel: 5,
-    email: 'claudecampos1@gmail.com'
+    email: 'claudecampos1@gmail.com',
+    password: '123',
+    faceCountAvg: 74,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 78,
+    wordsSpokenAvg: 180,
+    mouseClickAvg: 65,
+    keyStrokeAvg: 192,
+    numberOfSessions: 10
   },
   {
     firstName: 'Shahzaib',
     lastName: 'Malone',
     gradeLevel: 5,
-    email: 'shahzaibmalone1@gmail.com'
+    email: 'shahzaibmalone1@gmail.com',
+    password: '123',
+    faceCountAvg: 50,
+    faceAttemptAvg: 116,
+    faceScoreAvg: 43,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 337,
+    numberOfSessions: 10
   },
   {
     firstName: 'Kerry',
     lastName: 'Farley',
     gradeLevel: 5,
-    email: 'kerryfarley1@gmail.com'
+    email: 'kerryfarley1@gmail.com',
+    password: '123',
+    faceCountAvg: 91,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 87,
+    wordsSpokenAvg: 179,
+    mouseClickAvg: 83,
+    keyStrokeAvg: 270,
+    numberOfSessions: 10
   },
   {
     firstName: 'Donell',
     lastName: 'Simons',
     gradeLevel: 5,
-    email: 'donellsimons1@gmail.com'
+    email: 'donellsimons1@gmail.com',
+    password: '123',
+    faceCountAvg: 95,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 93,
+    wordsSpokenAvg: 275,
+    mouseClickAvg: 56,
+    keyStrokeAvg: 137,
+    numberOfSessions: 10
   },
   {
     firstName: 'Zakariyah',
     lastName: 'Tomlinson',
     gradeLevel: 5,
-    email: 'zakariyahtomlinson1@gmail.com'
+    email: 'zakariyahtomlinson1@gmail.com',
+    password: '123',
+    faceCountAvg: 78,
+    faceAttemptAvg: 119,
+    faceScoreAvg: 65,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 99,
+    keyStrokeAvg: 190,
+    numberOfSessions: 10
   },
   {
     firstName: 'Ubaid',
     lastName: 'Cervantes',
     gradeLevel: 5,
-    email: 'ubaidcervantes1@gmail.com'
+    email: 'ubaidcervantes1@gmail.com',
+    password: '123',
+    faceCountAvg: 50,
+    faceAttemptAvg: 116,
+    faceScoreAvg: 43,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 120,
+    numberOfSessions: 10
   },
   {
     firstName: 'Paul',
     lastName: 'Atkinson',
     gradeLevel: 5,
-    email: 'paulatkinson1@gmail.com'
+    email: 'paulatkinson1@gmail.com',
+    password: '123',
+    faceCountAvg: 52,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 68,
+    wordsSpokenAvg: 108,
+    mouseClickAvg: 59,
+    keyStrokeAvg: 243,
+    numberOfSessions: 10
   },
   {
     firstName: 'Xander',
     lastName: 'Callahan',
     gradeLevel: 5,
-    email: 'xandercallahan1@gmail.com'
+    email: 'xandercallahan1@gmail.com',
+    password: '123',
+    faceCountAvg: 79,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 92,
+    wordsSpokenAvg: 302,
+    mouseClickAvg: 83,
+    keyStrokeAvg: 218,
+    numberOfSessions: 10
   },
   {
     firstName: 'Millicent',
     lastName: 'Mathis',
     gradeLevel: 5,
-    email: 'millicentmathis1@gmail.com'
+    email: 'millicentmathis1@gmail.com',
+    password: '123',
+    faceCountAvg: 76,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 82,
+    wordsSpokenAvg: 319,
+    mouseClickAvg: 121,
+    keyStrokeAvg: 279,
+    numberOfSessions: 10
   },
   {
     firstName: 'Aaisha',
     lastName: 'Bruce',
     gradeLevel: 5,
-    email: 'aaishabruce1@gmail.com'
+    email: 'aaishabruce1@gmail.com',
+    password: '123',
+    faceCountAvg: 50,
+    faceAttemptAvg: 116,
+    faceScoreAvg: 43,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 120,
+    numberOfSessions: 10
   },
   {
     firstName: 'Uzma',
     lastName: 'Drummond',
     gradeLevel: 5,
-    email: 'uzmadrummond1@gmail.com'
+    email: 'uzmadrummond1@gmail.com',
+    password: '123',
+    faceCountAvg: 94,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 91,
+    wordsSpokenAvg: 292,
+    mouseClickAvg: 87,
+    keyStrokeAvg: 167,
+    numberOfSessions: 10
   },
   {
     firstName: 'Sullivan',
     lastName: 'Ireland',
     gradeLevel: 5,
-    email: 'sullivanireland1@gmail.com'
+    email: 'sullivanireland1@gmail.com',
+    password: '123',
+    faceCountAvg: 89,
+    faceAttemptAvg: 120,
+    faceScoreAvg: 43,
+    wordsSpokenAvg: 301,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 120,
+    numberOfSessions: 10
   },
   {
     firstName: 'Hamid',
     lastName: 'Oneal',
     gradeLevel: 5,
-    email: 'hamidoneal1@gmail.com'
+    email: 'hamidoneal1@gmail.com',
+    password: '123',
+    faceCountAvg: 69,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 73,
+    wordsSpokenAvg: 141,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 197,
+    numberOfSessions: 10
   },
   {
     firstName: 'Hailie',
     lastName: 'Jones',
     gradeLevel: 5,
-    email: 'hailiejones1@gmail.com'
+    email: 'hailiejones1@gmail.com',
+    password: '123',
+    faceCountAvg: 88,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 93,
+    wordsSpokenAvg: 249,
+    mouseClickAvg: 92,
+    keyStrokeAvg: 269,
+    numberOfSessions: 10
   },
   {
     firstName: 'Saeed',
     lastName: 'Davie',
     gradeLevel: 5,
-    email: 'saeeddavie1@gmail.com'
+    email: 'saeeddavie1@gmail.com',
+    password: '123',
+    faceCountAvg: 76,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 79,
+    wordsSpokenAvg: 178,
+    mouseClickAvg: 83,
+    keyStrokeAvg: 212,
+    numberOfSessions: 10
   },
   {
     firstName: 'Caiden',
     lastName: 'Connor',
     gradeLevel: 5,
-    email: 'caidenconnor1@gmail.com'
+    email: 'caidenconnor1@gmail.com',
+    password: '123',
+    faceCountAvg: 69,
+    faceAttemptAvg: 125,
+    faceScoreAvg: 73,
+    wordsSpokenAvg: 141,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 197,
+    numberOfSessions: 10
   },
   {
     firstName: 'Bjorn',
     lastName: 'Salt',
     gradeLevel: 5,
-    email: 'bjornsalt1@gmail.com'
+    email: 'bjornsalt1@gmail.com',
+    password: '123',
+    faceCountAvg: 78,
+    faceAttemptAvg: 118,
+    faceScoreAvg: 66,
+    wordsSpokenAvg: 141,
+    mouseClickAvg: 90,
+    keyStrokeAvg: 197,
+    numberOfSessions: 10
   }
   // {
   //   firstName: "Katarina",
---
+++ student-session-seed.js
@@ -0,0 +1,814 @@
+const studentSessionSeed = [
+  {
+    studentId: 1,
+    sessionId: 1,
+    faceCount: 72,
+    faceDetects: 125,
+    wordCount: 208,
+    clickCount: 58,
+    keyCount: 136
+  },
+  {
+    studentId: 2,
+    sessionId: 1,
+    faceCount: 109,
+    faceDetects: 125,
+    wordCount: 165,
+    clickCount: 125,
+    keyCount: 188
+  },
+  {
+    studentId: 3,
+    sessionId: 1,
+    faceCount: 102,
+    faceDetects: 125,
+    wordCount: 123,
+    clickCount: 54,
+    keyCount: 165
+  },
+  {
+    studentId: 4,
+    sessionId: 1,
+    faceCount: 72,
+    faceDetects: 125,
+    wordCount: 443,
+    clickCount: 60,
+    keyCount: 247
+  },
+  {
+    studentId: 5,
+    sessionId: 1,
+    faceCount: 74,
+    faceDetects: 125,
+    wordCount: 376,
+    clickCount: 124,
+    keyCount: 231
+  },
+  {
+    studentId: 6,
+    sessionId: 1,
+    faceCount: 89,
+    faceDetects: 125,
+    wordCount: 167,
+    clickCount: 44,
+    keyCount: 340
+  },
+  {
+    studentId: 7,
+    sessionId: 1,
+    faceCount: 106,
+    faceDetects: 125,
+    wordCount: 425,
+    clickCount: 108,
+    keyCount: 105
+  },
+  {
+    studentId: 8,
+    sessionId: 1,
+    faceCount: 75,
+    faceDetects: 125,
+    wordCount: 276,
+    clickCount: 81,
+    keyCount: 144
+  },
+  {
+    studentId: 9,
+    sessionId: 1,
+    faceCount: 52,
+    faceDetects: 125,
+    wordCount: 403,
+    clickCount: 46,
+    keyCount: 118
+  },
+  {
+    studentId: 10,
+    sessionId: 1,
+    faceCount: 69,
+    faceDetects: 125,
+    wordCount: 127,
+    clickCount: 64,
+    keyCount: 158
+  },
+  {
+    studentId: 11,
+    sessionId: 1,
+    faceCount: 98,
+    faceDetects: 125,
+    wordCount: 284,
+    clickCount: 118,
+    keyCount: 229
+  },
+  {
+    studentId: 12,
+    sessionId: 1,
+    faceCount: 53,
+    faceDetects: 125,
+    wordCount: 179,
+    clickCount: 76,
+    keyCount: 124
+  },
+  {
+    studentId: 13,
+    sessionId: 1,
+    faceCount: 89,
+    faceDetects: 125,
+    wordCount: 346,
+    clickCount: 76,
+    keyCount: 237
+  },
+  {
+    studentId: 14,
+    sessionId: 1,
+    faceCount: 107,
+    faceDetects: 125,
+    wordCount: 433,
+    clickCount: 73,
+    keyCount: 110
+  },
+  {
+    studentId: 15,
+    sessionId: 1,
+    faceCount: 91,
+    faceDetects: 125,
+    wordCount: 369,
+    clickCount: 26,
+    keyCount: 178
+  },
+  {
+    studentId: 16,
+    sessionId: 1,
+    faceCount: 102,
+    faceDetects: 125,
+    wordCount: 432,
+    clickCount: 24,
+    keyCount: 108
+  },
+  {
+    studentId: 17,
+    sessionId: 1,
+    faceCount: 74,
+    faceDetects: 125,
+    wordCount: 297,
+    clickCount: 81,
+    keyCount: 123
+  },
+  {
+    studentId: 18,
+    sessionId: 1,
+    faceCount: 102,
+    faceDetects: 125,
+    wordCount: 258,
+    clickCount: 124,
+    keyCount: 127
+  },
+  {
+    studentId: 19,
+    sessionId: 1,
+    faceCount: 76,
+    faceDetects: 125,
+    wordCount: 399,
+    clickCount: 68,
+    keyCount: 336
+  },
+  {
+    studentId: 20,
+    sessionId: 1,
+    faceCount: 80,
+    faceDetects: 125,
+    wordCount: 149,
+    clickCount: 89,
+    keyCount: 326
+  },
+  {
+    studentId: 21,
+    sessionId: 1,
+    faceCount: 101,
+    faceDetects: 125,
+    wordCount: 197,
+    clickCount: 82,
+    keyCount: 239
+  },
+  {
+    studentId: 22,
+    sessionId: 1,
+    faceCount: 89,
+    faceDetects: 125,
+    wordCount: 149,
+    clickCount: 85,
+    keyCount: 323
+  },
+  {
+    studentId: 23,
+    sessionId: 1,
+    faceCount: 59,
+    faceDetects: 125,
+    wordCount: 278,
+    clickCount: 22,
+    keyCount: 303
+  },
+  {
+    studentId: 24,
+    sessionId: 1,
+    faceCount: 106,
+    faceDetects: 125,
+    wordCount: 250,
+    clickCount: 63,
+    keyCount: 251
+  },
+  {
+    studentId: 25,
+    sessionId: 1,
+    faceCount: 71,
+    faceDetects: 125,
+    wordCount: 384,
+    clickCount: 105,
+    keyCount: 343
+  },
+  {
+    studentId: 26,
+    sessionId: 1,
+    faceCount: 93,
+    faceDetects: 125,
+    wordCount: 187,
+    clickCount: 75,
+    keyCount: 171
+  },
+  {
+    studentId: 27,
+    sessionId: 1,
+    faceCount: 59,
+    faceDetects: 125,
+    wordCount: 146,
+    clickCount: 34,
+    keyCount: 257
+  },
+  {
+    studentId: 28,
+    sessionId: 1,
+    faceCount: 80,
+    faceDetects: 125,
+    wordCount: 202,
+    clickCount: 114,
+    keyCount: 102
+  },
+  {
+    studentId: 29,
+    sessionId: 1,
+    faceCount: 110,
+    faceDetects: 125,
+    wordCount: 435,
+    clickCount: 61,
+    keyCount: 315
+  },
+  {
+    studentId: 30,
+    sessionId: 1,
+    faceCount: 66,
+    faceDetects: 125,
+    wordCount: 225,
+    clickCount: 26,
+    keyCount: 275
+  },
+  {
+    studentId: 1,
+    sessionId: 2,
+    faceCount: 106,
+    faceDetects: 125,
+    wordCount: 131,
+    clickCount: 106,
+    keyCount: 134
+  },
+  {
+    studentId: 2,
+    sessionId: 2,
+    faceCount: 77,
+    faceDetects: 125,
+    wordCount: 381,
+    clickCount: 90,
+    keyCount: 282
+  },
+  {
+    studentId: 3,
+    sessionId: 2,
+    faceCount: 84,
+    faceDetects: 125,
+    wordCount: 330,
+    clickCount: 31,
+    keyCount: 264
+  },
+  {
+    studentId: 4,
+    sessionId: 2,
+    faceCount: 84,
+    faceDetects: 125,
+    wordCount: 277,
+    clickCount: 118,
+    keyCount: 164
+  },
+  {
+    studentId: 5,
+    sessionId: 2,
+    faceCount: 58,
+    faceDetects: 125,
+    wordCount: 439,
+    clickCount: 59,
+    keyCount: 162
+  },
+  {
+    studentId: 6,
+    sessionId: 2,
+    faceCount: 81,
+    faceDetects: 125,
+    wordCount: 211,
+    clickCount: 32,
+    keyCount: 180
+  },
+  {
+    studentId: 7,
+    sessionId: 2,
+    faceCount: 80,
+    faceDetects: 125,
+    wordCount: 448,
+    clickCount: 110,
+    keyCount: 186
+  },
+  {
+    studentId: 8,
+    sessionId: 2,
+    faceCount: 75,
+    faceDetects: 125,
+    wordCount: 248,
+    clickCount: 61,
+    keyCount: 237
+  },
+  {
+    studentId: 9,
+    sessionId: 2,
+    faceCount: 93,
+    faceDetects: 125,
+    wordCount: 405,
+    clickCount: 78,
+    keyCount: 146
+  },
+  {
+    studentId: 10,
+    sessionId: 2,
+    faceCount: 59,
+    faceDetects: 125,
+    wordCount: 252,
+    clickCount: 92,
+    keyCount: 146
+  },
+  {
+    studentId: 11,
+    sessionId: 2,
+    faceCount: 51,
+    faceDetects: 125,
+    wordCount: 115,
+    clickCount: 64,
+    keyCount: 103
+  },
+  {
+    studentId: 12,
+    sessionId: 2,
+    faceCount: 105,
+    faceDetects: 125,
+    wordCount: 318,
+    clickCount: 96,
+    keyCount: 241
+  },
+  {
+    studentId: 13,
+    sessionId: 2,
+    faceCount: 91,
+    faceDetects: 125,
+    wordCount: 284,
+    clickCount: 28,
+    keyCount: 330
+  },
+  {
+    studentId: 14,
+    sessionId: 2,
+    faceCount: 65,
+    faceDetects: 125,
+    wordCount: 188,
+    clickCount: 43,
+    keyCount: 335
+  },
+  {
+    studentId: 15,
+    sessionId: 2,
+    faceCount: 101,
+    faceDetects: 125,
+    wordCount: 257,
+    clickCount: 49,
+    keyCount: 125
+  },
+  {
+    studentId: 16,
+    sessionId: 2,
+    faceCount: 71,
+    faceDetects: 125,
+    wordCount: 262,
+    clickCount: 102,
+    keyCount: 271
+  },
+  {
+    studentId: 17,
+    sessionId: 2,
+    faceCount: 109,
+    faceDetects: 125,
+    wordCount: 385,
+    clickCount: 58,
+    keyCount: 218
+  },
+  {
+    studentId: 18,
+    sessionId: 2,
+    faceCount: 73,
+    faceDetects: 125,
+    wordCount: 276,
+    clickCount: 79,
+    keyCount: 291
+  },
+  {
+    studentId: 19,
+    sessionId: 2,
+    faceCount: 98,
+    faceDetects: 125,
+    wordCount: 365,
+    clickCount: 94,
+    keyCount: 236
+  },
+  {
+    studentId: 20,
+    sessionId: 2,
+    faceCount: 107,
+    faceDetects: 125,
+    wordCount: 157,
+    clickCount: 42,
+    keyCount: 105
+  },
+  {
+    studentId: 21,
+    sessionId: 2,
+    faceCount: 77,
+    faceDetects: 125,
+    wordCount: 240,
+    clickCount: 108,
+    keyCount: 173
+  },
+  {
+    studentId: 22,
+    sessionId: 2,
+    faceCount: 73,
+    faceDetects: 125,
+    wordCount: 148,
+    clickCount: 57,
+    keyCount: 346
+  },
+  {
+    studentId: 23,
+    sessionId: 2,
+    faceCount: 79,
+    faceDetects: 125,
+    wordCount: 291,
+    clickCount: 120,
+    keyCount: 275
+  },
+  {
+    studentId: 24,
+    sessionId: 2,
+    faceCount: 62,
+    faceDetects: 125,
+    wordCount: 206,
+    clickCount: 102,
+    keyCount: 318
+  },
+  {
+    studentId: 25,
+    sessionId: 2,
+    faceCount: 98,
+    faceDetects: 125,
+    wordCount: 312,
+    clickCount: 75,
+    keyCount: 151
+  },
+  {
+    studentId: 26,
+    sessionId: 2,
+    faceCount: 74,
+    faceDetects: 125,
+    wordCount: 206,
+    clickCount: 112,
+    keyCount: 100
+  },
+  {
+    studentId: 27,
+    sessionId: 2,
+    faceCount: 68,
+    faceDetects: 125,
+    wordCount: 329,
+    clickCount: 50,
+    keyCount: 255
+  },
+  {
+    studentId: 28,
+    sessionId: 2,
+    faceCount: 54,
+    faceDetects: 125,
+    wordCount: 307,
+    clickCount: 122,
+    keyCount: 138
+  },
+  {
+    studentId: 29,
+    sessionId: 2,
+    faceCount: 66,
+    faceDetects: 125,
+    wordCount: 320,
+    clickCount: 30,
+    keyCount: 271
+  },
+  {
+    studentId: 30,
+    sessionId: 2,
+    faceCount: 109,
+    faceDetects: 125,
+    wordCount: 389,
+    clickCount: 41,
+    keyCount: 112
+  },
+  {
+    studentId: 1,
+    sessionId: 3,
+    faceCount: 97,
+    faceDetects: 125,
+    wordCount: 368,
+    clickCount: 109,
+    keyCount: 100
+  },
+  {
+    studentId: 2,
+    sessionId: 3,
+    faceCount: 81,
+    faceDetects: 125,
+    wordCount: 373,
+    clickCount: 110,
+    keyCount: 141
+  },
+  {
+    studentId: 3,
+    sessionId: 3,
+    faceCount: 76,
+    faceDetects: 125,
+    wordCount: 101,
+    clickCount: 69,
+    keyCount: 316
+  },
+  {
+    studentId: 4,
+    sessionId: 3,
+    faceCount: 52,
+    faceDetects: 125,
+    wordCount: 319,
+    clickCount: 98,
+    keyCount: 296
+  },
+  {
+    studentId: 5,
+    sessionId: 3,
+    faceCount: 104,
+    faceDetects: 125,
+    wordCount: 219,
+    clickCount: 29,
+    keyCount: 106
+  },
+  {
+    studentId: 6,
+    sessionId: 3,
+    faceCount: 54,
+    faceDetects: 125,
+    wordCount: 291,
+    clickCount: 33,
+    keyCount: 234
+  },
+  {
+    studentId: 7,
+    sessionId: 3,
+    faceCount: 54,
+    faceDetects: 125,
+    wordCount: 281,
+    clickCount: 24,
+    keyCount: 265
+  },
+  {
+    studentId: 8,
+    sessionId: 3,
+    faceCount: 92,
+    faceDetects: 125,
+    wordCount: 299,
+    clickCount: 107,
+    keyCount: 299
+  },
+  {
+    studentId: 9,
+    sessionId: 3,
+    faceCount: 109,
+    faceDetects: 125,
+    wordCount: 278,
+    clickCount: 97,
+    keyCount: 223
+  },
+  {
+    studentId: 10,
+    sessionId: 3,
+    faceCount: 93,
+    faceDetects: 125,
+    wordCount: 232,
+    clickCount: 50,
+    keyCount: 147
+  },
+  {
+    studentId: 11,
+    sessionId: 3,
+    faceCount: 76,
+    faceDetects: 125,
+    wordCount: 169,
+    clickCount: 101,
+    keyCount: 192
+  },
+  {
+    studentId: 12,
+    sessionId: 3,
+    faceCount: 110,
+    faceDetects: 125,
+    wordCount: 442,
+    clickCount: 58,
+    keyCount: 189
+  },
+  {
+    studentId: 13,
+    sessionId: 3,
+    faceCount: 100,
+    faceDetects: 125,
+    wordCount: 329,
+    clickCount: 120,
+    keyCount: 327
+  },
+  {
+    studentId: 14,
+    sessionId: 3,
+    faceCount: 69,
+    faceDetects: 125,
+    wordCount: 246,
+    clickCount: 83,
+    keyCount: 136
+  },
+  {
+    studentId: 15,
+    sessionId: 3,
+    faceCount: 96,
+    faceDetects: 125,
+    wordCount: 345,
+    clickCount: 68,
+    keyCount: 276
+  },
+  {
+    studentId: 16,
+    sessionId: 3,
+    faceCount: 104,
+    faceDetects: 125,
+    wordCount: 167,
+    clickCount: 34,
+    keyCount: 272
+  },
+  {
+    studentId: 17,
+    sessionId: 3,
+    faceCount: 100,
+    faceDetects: 125,
+    wordCount: 241,
+    clickCount: 73,
+    keyCount: 216
+  },
+  {
+    studentId: 18,
+    sessionId: 3,
+    faceCount: 109,
+    faceDetects: 125,
+    wordCount: 274,
+    clickCount: 71,
+    keyCount: 324
+  },
+  {
+    studentId: 19,
+    sessionId: 3,
+    faceCount: 62,
+    faceDetects: 125,
+    wordCount: 245,
+    clickCount: 31,
+    keyCount: 153
+  },
+  {
+    studentId: 20,
+    sessionId: 3,
+    faceCount: 53,
+    faceDetects: 125,
+    wordCount: 358,
+    clickCount: 48,
+    keyCount: 130
+  },
+  {
+    studentId: 21,
+    sessionId: 3,
+    faceCount: 73,
+    faceDetects: 125,
+    wordCount: 424,
+    clickCount: 123,
+    keyCount: 114
+  },
+  {
+    studentId: 22,
+    sessionId: 3,
+    faceCount: 81,
+    faceDetects: 125,
+    wordCount: 307,
+    clickCount: 82,
+    keyCount: 167
+  },
+  {
+    studentId: 23,
+    sessionId: 3,
+    faceCount: 90,
+    faceDetects: 125,
+    wordCount: 174,
+    clickCount: 82,
+    keyCount: 198
+  },
+  {
+    studentId: 24,
+    sessionId: 3,
+    faceCount: 98,
+    faceDetects: 125,
+    wordCount: 382,
+    clickCount: 87,
+    keyCount: 272
+  },
+  {
+    studentId: 25,
+    sessionId: 3,
+    faceCount: 92,
+    faceDetects: 125,
+    wordCount: 174,
+    clickCount: 52,
+    keyCount: 313
+  },
+  {
+    studentId: 26,
+    sessionId: 3,
+    faceCount: 87,
+    faceDetects: 125,
+    wordCount: 218,
+    clickCount: 90,
+    keyCount: 296
+  },
+  {
+    studentId: 27,
+    sessionId: 3,
+    faceCount: 83,
+    faceDetects: 125,
+    wordCount: 406,
+    clickCount: 116,
+    keyCount: 239
+  },
+  {
+    studentId: 28,
+    sessionId: 3,
+    faceCount: 68,
+    faceDetects: 125,
+    wordCount: 311,
+    clickCount: 78,
+    keyCount: 197
+  },
+  {
+    studentId: 29,
+    sessionId: 3,
+    faceCount: 54,
+    faceDetects: 125,
+    wordCount: 207,
+    clickCount: 83,
+    keyCount: 156
+  },
+  {
+    studentId: 30,
+    sessionId: 3,
+    faceCount: 65,
+    faceDetects: 125,
+    wordCount: 369,
+    clickCount: 45,
+    keyCount: 101
+  }
+]
+
+module.exports = studentSessionSeed
