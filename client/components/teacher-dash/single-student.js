f3c947804cf41593891c5a9aa6da0fa86820d629
Barish <barpoole@gmail.com>
2020-07-14 16:03:23 -0400

    fix: tables and charts are conditional on having present data

--- client/components/teacher-dash/single-student.js
+++ client/components/teacher-dash/single-student.js
@@ -8,18 +8,18 @@ class SingleStudent extends React.Component {
     const {liveStudents, liveSession, studentId} = this.props
 
     const student = liveStudents.find(
-      liveStudent => liveStudent.id === studentId
+      (liveStudent) => liveStudent.id === studentId
     )
 
     return (
       <div>
         <Grid item xs={12} container direction="row">
           <Grid item xs={6}>
-            <Table data={student} />
+            {student && <Table data={student} />}
           </Grid>
           <Grid item xs={6}>
             <div className="chart">
-              <Chart data={student} compare={liveSession} />
+              {student && <Chart data={student} compare={liveSession} />}
             </div>
           </Grid>
         </Grid>
@@ -28,10 +28,10 @@ class SingleStudent extends React.Component {
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     liveStudents: state.liveStudents,
-    liveSession: state.liveSession
+    liveSession: state.liveSession,
   }
 }
 
